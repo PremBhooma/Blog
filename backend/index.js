da4fb01aad4e3258dcbf7a16d2a965376a0a2c80
Prem Bhooma <bhoomasagar1213@gmail.com>
2023-09-23 14:54:04 +0530

    DB Created

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,20 @@
 const express = require("express")
 
+const { connection } = require("mongoose")
+
 const app = express()
 
 app.get("/", (req, res) => {
     res.send("Base Port Welcomes You")
 })
 
-app.listen(8090, (req, res) => {
+app.listen(8090, async (req, res) => {
+    try {
+        await connection
+        console.log("DB Connection Success")
+    } catch (err) {
+        console.log("Db Connection Failed")
+        console.log(err)
+    }
     console.log("listening on 8090 Port")
 })
