5147adc72097937367a4ae91725bcf8be75e1f1b
Prem Bhooma <bhoomasagar1213@gmail.com>
2023-09-23 15:12:00 +0530

    signupStarted

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,16 @@ const express = require("express")
 const { connection } = require("mongoose")
 
 const app = express()
+app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("Base Port Welcomes You")
+})
+
+app.post("/signup", (req, res) => {
+    const { name, phone_number, email, password } = req.body
+    console.log(req.body)
+    res.send({ msg: `Signup Succesfully` })
 })
 
 app.listen(8090, async (req, res) => {
