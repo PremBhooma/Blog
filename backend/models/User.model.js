a408bb3ce74b6264aaeb7e2eaee658c98f783c5a
Prem Bhooma <bhoomasagar1213@gmail.com>
2023-09-25 11:46:52 +0530

    ModelDone

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose")
 
 const userScheme = new mongoose.Schema({
-
+    name: { type: String, required: true },
+    phone_number: { type: Number, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true }
 })
 
 const UserModel = mongoose.model("user", userScheme)
+
+module.exports = {
+    UserModel
+}
